0d126e5af0a3192b348b85bffbb8f662339f569b
shreyank-SP <53818112+shreyank-SP@users.noreply.github.com>
2019-08-07 12:25:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+#include <stdlib.h>
+int  main()
+{  int N=0,i,max,arr[10];
+   printf("enter the Number of integers :");
+   scanf("%d",&N);
+   printf("enter the values");
+   for(i=0;i<N;i++)
+   {
+	   scanf("%d",&arr[i]);
+   }
+   max=arr[0];
+   for(i=0;i<N;i++)
+   {   
+	   if(max<arr[i])
+	   {
+		   max=arr[i];
+	   }
+   }
+   printf("maximum element is %d \n",max);
+   return 0;
+}
