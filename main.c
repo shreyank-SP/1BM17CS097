9086bcd9db2b9cef027212f9fe3f23b95be36942
shreyank-SP <53818112+shreyank-SP@users.noreply.github.com>
2019-08-07 13:17:12 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,46 @@
-
 #include <stdio.h>
 
-#include <stdlib.h>
-int  main()
-{  int N=0,i,max,arr[10];
-   printf("enter the Number of integers :");
-   scanf("%d",&N);
-   printf("enter the values");
-   for(i=0;i<N;i++)
+void main()
+{   int tc,i,j,N,f,l,ele,mid,arr[10],flag=0;
+	printf("enter the number of test cases");
+	scanf("%d",&tc);
+	printf("\n");
+	for(j=0;j<tc;j++)
+	{
+	printf("enter the number of elements in the array and element to find in the array");
+	scanf("%d%d",&N,&ele);
+	printf("enter the array elements");
+	   for(i=0;i<N;i++)
    {
 	   scanf("%d",&arr[i]);
    }
-   max=arr[0];
-   for(i=0;i<N;i++)
-   {   
-	   if(max<arr[i])
-	   {
-		   max=arr[i];
-	   }
-   }
-   printf("maximum element is %d \n",max);
-   return 0;
+   
+	f=0;l=N;
+    
+	while(f<=l)
+	{ 	mid=(f+l)/2;
+	  if(ele==arr[mid])
+	  {  flag=1;
+	  printf("1");
+		break;
+	
+	  }
+	  else if(ele>arr[mid])
+	  {
+		  f=mid+1;
+	  }	
+	  else
+	  {
+		  l=mid-1;
+	  }
+	  
+	}
+	
+	if(flag==0)
+	{
+		printf("-1\n");
+	}
+	
+	
+ }
 }
