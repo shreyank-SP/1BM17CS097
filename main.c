c276bfcd4689bb76b7db5a81ba7c90bb39e53db0
shreyank-SP <53818112+shreyank-SP@users.noreply.github.com>
2019-08-09 08:32:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 
 void main()
 {   int tc,i,j,N,f,l,ele,mid,arr[10],flag=0;
-	printf("enter the number of test cases");
+	
 	scanf("%d",&tc);
 	printf("\n");
 	for(j=0;j<tc;j++)
 	{
-	printf("enter the number of elements in the array and element to find in the array");
+
 	scanf("%d%d",&N,&ele);
-	printf("enter the array elements");
 	   for(i=0;i<N;i++)
    {
 	   scanf("%d",&arr[i]);
