58c3eabd3a51f63c468bc6259c11bc4a2ff6c919
shreyank-SP <53818112+shreyank-SP@users.noreply.github.com>
2019-08-13 15:44:56 +0530

    Update lab1b.py

--- lab1b.py
+++ lab1b.py
@@ -6,7 +6,10 @@ def fibo(n):
     elif n==2:
          print(str(a)+" , " +str(b),end="");
     else :
-         print(str(a)+" , "+str(b),end="")
+         if(n<=0):
+              print("Incorrect nummber ")
+         else :
+              print(str(a)+" , "+str(b),end="")
     for i in range(2,int(n)):
              sum=a+b
              a=b
