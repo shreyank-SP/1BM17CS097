65e744a52dd7459fdccfd9ad001402af12a3f177
shreyank-SP <53818112+shreyank-SP@users.noreply.github.com>
2019-08-13 15:22:58 +0530

    Add files via upload

---
+++ lab1b.py
@@ -0,0 +1,17 @@
+def fibo(n):
+    a=0;
+    b=1;
+    if n==1:
+        print(a);
+    elif n==2:
+         print(str(a)+" , " +str(b),end="");
+    else :
+         print(str(a)+" , "+str(b),end="")
+    for i in range(2,int(n)):
+             sum=a+b
+             a=b
+             b=sum
+             print(" , "+str(sum),end="");
+n=input("Enter a number of fibonacci series to find : ")
+fibo(int(n));  
+             
